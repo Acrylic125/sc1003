1871364f9053341d4d7617b1ada0fbb996339037
Acrylic125 <acrylic125email@gmail.com>
2024-10-09 13:02:44 +0800

    done

--- week 8/lab.py
+++ week 8/lab.py
@@ -1,7 +1,7 @@
 import random
 
 def get_ab_score(guess: str, answer: str) -> tuple[int, int]:
-    if len(guess) != len(answer) != 4:
+    if len(guess) != 4 or len(answer) != 4:
         raise ValueError("Guess and answer must be 4 digits long")
     
     a = 0
@@ -10,21 +10,28 @@ def get_ab_score(guess: str, answer: str) -> tuple[int, int]:
     if guess == answer:
         return 4, 0
 
+    utilised = []
     for i in range(len(guess)):
         guess_at_i = guess[i]
         answer_at_i = answer[i]
         
-        if guess_at_i == answer_at_i:
-            a += 1
-        elif answer.find(guess_at_i) != -1:
-            b += 1
+        if guess_at_i not in utilised:
+            if guess_at_i == answer_at_i:
+                a += 1
+            elif guess_at_i in answer:
+            # elif answer.find(guess_at_i) != -1:
+                b += 1
+            utilised.append(guess_at_i)
     
     return a, b
 
+def generate_answer():
+    digits = [i for i in range(10)]
+    random.shuffle(digits)
+    answer = "".join(map(str, digits[0:4]))
+    return answer
 
-digits = [i for i in range(10)]
-random.shuffle(digits)
-answer = "".join(map(str, digits[0:4]))
+answer = generate_answer()
 print(f"Answer: {answer}")
 
 attempts = 0
