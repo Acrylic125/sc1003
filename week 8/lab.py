049ecd417897a4aa18428a68dd1946dc4f97cead
Acrylic125 <acrylic125email@gmail.com>
2024-10-09 12:51:11 +0800

    fix solution to use rand digits with no repeats

---
+++ week 8/lab.py
@@ -0,0 +1,42 @@
+import random
+
+def get_ab_score(guess: str, answer: str) -> tuple[int, int]:
+    if len(guess) != len(answer) != 4:
+        raise ValueError("Guess and answer must be 4 digits long")
+    
+    a = 0
+    b = 0
+
+    if guess == answer:
+        return 4, 0
+
+    for i in range(len(guess)):
+        guess_at_i = guess[i]
+        answer_at_i = answer[i]
+        
+        if guess_at_i == answer_at_i:
+            a += 1
+        elif answer.find(guess_at_i) != -1:
+            b += 1
+    
+    return a, b
+
+
+digits = [i for i in range(10)]
+random.shuffle(digits)
+answer = "".join(map(str, digits[0:4]))
+print(f"Answer: {answer}")
+
+attempts = 0
+while True:
+    guess = input("Enter your guess: ")
+
+    try:
+        attempts += 1
+        a, b = get_ab_score(guess, answer)
+        if a == 4:
+            print(f"Correct! You guessed the answer in {attempts} attempts.")
+            break
+        print(f"Guess: {guess}\nA: {a}\nB: {b}")
+    except ValueError as e:
+        print("Invalid input. Make sure the guess is 4 digits long.")
